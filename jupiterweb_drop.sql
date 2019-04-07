4536e8e8829f07954bf43a2a6c715a38d028525d
LucasGasparino <lucas.gasparino@usp.br>
2019-04-07 12:24:05 -0300

    Drop tables, types and function script

---
+++ jupiterweb_drop.sql
@@ -0,0 +1,41 @@
+﻿drop table if exists pf_se cascade;
+
+drop table if exists "Planeja" cascade;
+
+drop table if exists rel_cur_tri cascade;
+
+drop table if exists "Trilha" cascade;
+
+drop table if exists us_pf cascade;
+
+drop table if exists "Perfil" cascade;
+
+drop table if exists usuario cascade;
+
+drop table if exists "Administra" cascade;
+
+drop table if exists "Administrador" cascade;
+
+drop table if exists curriculo cascade;
+
+drop table if exists "Cursa" cascade;
+
+drop table if exists "Aluno" cascade;
+
+drop table if exists "Ministra" cascade;
+
+drop table if exists "Professor" cascade;
+
+drop table if exists rel_dis_mod cascade;
+
+drop table if exists "Disciplina" cascade;
+
+drop table if exists modulo cascade;
+
+drop table if exists servico cascade;
+
+drop table if exists "Pessoa" cascade;
+
+drop function if exists cpf_validar(varchar);
+
+drop type if exists endereco;
